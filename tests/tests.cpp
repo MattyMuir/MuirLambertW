a5c102e114d02a4f58d3da775244bd48152f4425
Matty Muir <matty.muir@btinternet.com>
2024-10-19 02:05:28 +0100

    New series for large arguments in W0.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -15,47 +15,8 @@
 
 int main()
 {
-	/*
 	static std::mt19937_64 gen{ std::random_device{}() };
-	std::uniform_real_distribution<double> dist{ EM_UP, -0.25 };
+	ReciprocalDistributionEx dist{ EM_UP, Infinity, false };
 
-	double worst = -1;
-	for (;;)
-	{
-		double x = dist(gen);
-		auto [inf, sup] = ReferenceLambertW0(x);
-
-		double approx = MuirW0(x);
-
-		uint64_t err = std::max(ULPDistance(approx, inf), ULPDistance(approx, sup));
-
-		if (err <= 4)
-			continue;
-
-		if (x > worst)
-		{
-			worst = x;
-			dist = std::uniform_real_distribution<double>{ worst, -0.25 };
-			std::cout << std::format("{}\n", worst);
-		}
-	}
-	*/
-
-	static std::mt19937_64 gen{ std::random_device{}() };
-	static std::uniform_real_distribution<double> dist{ EM_UP, 0 };
-
-	size_t NumData = 10000;
-
-	std::vector<double> data;
-	for (size_t i = 0; i < NumData; i++)
-		data.push_back(dist(gen));
-
-	auto start = std::chrono::steady_clock::now();
-	double _ = 0.0;
-	for (double d : data)
-		_ += ReferenceLambertWm1(d).inf;
-	auto end = std::chrono::steady_clock::now();
-
-	std::cout << _ << '\n';
-	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start) << '\n';
+	MaxULPRounded(ReferenceLambertW0, MakeSerial<MuirW0>, [&]() { return dist(gen); });
 }
